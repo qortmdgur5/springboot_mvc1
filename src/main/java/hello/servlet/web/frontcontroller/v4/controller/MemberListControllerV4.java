e75fa7df3c1ef98c7fa0846a2a87fca16f33b28c
Bsh <dnfg12@naver.com>
2022-06-03 18:51:03 +0900

    2020-06-03 퇴근 커밋 2

--- src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -4,21 +4,18 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.v4.ControllerV4;
 
+import java.util.List;
 import java.util.Map;
 
-public class MemberSaveControllerV4 implements ControllerV4 {
+public class MemberListControllerV4 implements ControllerV4 {
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
     public String process(Map<String, String> paramMap, Map<String, Object> model) {
-        String username = paramMap.get("username");
-        int age = Integer.parseInt(paramMap.get("age"));
 
-        Member member = new Member(username, age);
-        memberRepository.save(member);
-
-        model.put("member", member);
-        return "save-result";
+        List<Member> members = memberRepository.findAll();
+        model.put("members", members);
+        return "members";
     }
 }
